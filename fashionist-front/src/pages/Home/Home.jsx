1295a32e7ee7012f9985c404606fc62e9eb46c7b
Nathan Gavenski <nathangavenski@gmail.com>
2022-05-29 15:42:20 -0300

    Fix indent

--- fashionist-front/src/pages/Home/Home.jsx
+++ fashionist-front/src/pages/Home/Home.jsx
@@ -9,39 +9,39 @@ import FloatingActionButton from "../../components/FloatingActionButton";
 import { Grid, Group, Container } from "@mantine/core";
 
 function template(clothes) {
-    return (
-        <div className="home">
-            <Group>
-                <Navbar />
-                <Container fluid className="home-content">
-                    <Group className="content" direction="column">
-                        <h1 className="content-title">Home</h1>
-                        <SearchBar />
-                        <div className="content-content">
-                            <Grid>
-                                {
-                                    clothes.map(c => {
-                                        return (
-                                            <Grid.Col span={3}>
-                                                <Product
-                                                    id={c._id}
-                                                    liked={c.isLiked}
-                                                    url={c.url}
-                                                    isLikeable
-                                                    key={c._id}
-                                                />
-                                            </Grid.Col>
-                                        )
-                                    })
-                                }
-                            </Grid>
-                        </div>
-                    </Group>
-                </Container>
-            </Group>
-            <FloatingActionButton />
-        </div>
-    );
+  return (
+    <div className="home">
+      <Group>
+        <Navbar />
+        <Container fluid className="home-content">
+          <Group className="content" direction="column">
+            <h1 className="content-title">Home</h1>
+            <SearchBar />
+            <div className="content-content">
+              <Grid>
+                {
+                  clothes.map(c => {
+                    return (
+                      <Grid.Col span={3}>
+                        <Product
+                          id={c._id}
+                          liked={c.isLiked}
+                          url={c.url}
+                          isLikeable
+                          key={c._id}
+                        />
+                      </Grid.Col>
+                    )
+                  })
+                }
+              </Grid>
+            </div>
+          </Group>
+        </Container>
+      </Group>
+      <FloatingActionButton />
+    </div>
+  );
 };
 
 export default template;
